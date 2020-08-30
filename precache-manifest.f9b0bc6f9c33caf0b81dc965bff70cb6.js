d87ea9775ba7359caf163f5027ef772ddfd16386
ayaulym <ayaulymalzhan@gmail.com>
2020-08-30 11:49:39 +0600

    Updates

---
+++ precache-manifest.f9b0bc6f9c33caf0b81dc965bff70cb6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "468f255868a33c228c1871a23e736176",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0e52e6871322428636b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c73d7dc50b00161e7b31",
+    "url": "/static/js/2.0b97e698.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.0b97e698.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0e52e6871322428636b",
+    "url": "/static/js/main.0c36f177.chunk.js"
+  },
+  {
+    "revision": "1ca56b6e53ce1dc83ae6",
+    "url": "/static/js/runtime-main.0a3c20bd.js"
+  },
+  {
+    "revision": "845d9609e2a21fe20deb9c562953cb60",
+    "url": "/static/media/addUser.845d9609.svg"
+  },
+  {
+    "revision": "add5ed0bb55a49331781013028479c09",
+    "url": "/static/media/documents.add5ed0b.svg"
+  },
+  {
+    "revision": "26c86ebce2b33a6322783b18f2df0efd",
+    "url": "/static/media/knowledgeBase.26c86ebc.svg"
+  },
+  {
+    "revision": "cc750493e6cae9901ac90f8b2a529497",
+    "url": "/static/media/meetings.cc750493.svg"
+  },
+  {
+    "revision": "cbfd419a9939c1fc0968995863c9ebaf",
+    "url": "/static/media/numbers.cbfd419a.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/beeline-react/precache-manifest.66a1db1c846be383a4ee9490a916f4ce.js"
+  "/precache-manifest.f9b0bc6f9c33caf0b81dc965bff70cb6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/beeline-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
